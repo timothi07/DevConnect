970200f2208bc87d99446a10bc87e5c404137afb
Timothi M Abey <timothimabey@gmail.com>
2024-03-20 19:31:35 +0530

    db-completed

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 projectsList = [
         {
@@ -20,14 +21,12 @@ projectsList = [
 ]
 
 def projects(request):
-    page = 'projects'
-    number = 11
-    context = {'page' : page, 'number':number, 'projects':projectsList}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request,pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id = pk)
+    tags = projectObj.tags.all()
+    print('projectObj:', projectObj)
     return render(request, 'projects/single-project.html', {'project': projectObj})
