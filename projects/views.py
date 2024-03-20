e1e42d24f2ff51b712b8520c7c6da0adbf184a18
Timothi M Abey <timothimabey@gmail.com>
2024-03-20 08:54:44 +0530

    db-started

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+        {
+            'id': '1',
+            'title':"Ecommerece website",
+            'description':'Fully functional ecommerece website'
+        },
+        {
+            'id':'2',
+            'title':"Portfolio website",
+            'description':"This was a project where I"
+        },
+        {
+            'id':'3',
+            'title':"Social Network",
+            'description':"Awosome open source project"
+        },
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 11
+    context = {'page' : page, 'number':number, 'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request,pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
