9a491e883aca5fdb1bef6c400235ae13adff9d45
Timothi M Abey <timothimabey@gmail.com>
2024-03-22 15:22:02 +0530

    static_files-completed

--- devconnect/settings.py
+++ devconnect/settings.py
@@ -121,6 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
