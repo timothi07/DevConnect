d933153a899400f3b2128a6f45a7be2f018e6159
Timothi M Abey <timothimabey@gmail.com>
2024-04-17 16:05:01 +0530

    user-app started

--- devconnect/urls.py
+++ devconnect/urls.py
@@ -4,7 +4,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('projects.urls'))
+    path('projects/',include('projects.urls')),
+    path('',include('users.urls')),
 
 ]
 
